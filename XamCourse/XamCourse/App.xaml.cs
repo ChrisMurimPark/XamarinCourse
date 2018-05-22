d12323b0f442c3942305c3f0d2ec5d7d287e6ebb
Chris (Murim) Park <chrispark@wustl.edu>
2018-05-21 22:44:50 -0500

    Adding StackLayoutPage

--- XamCourse/XamCourse/App.xaml.cs
+++ XamCourse/XamCourse/App.xaml.cs
@@ -22,7 +22,7 @@ namespace XamCourse
             //else
                 //DependencyService.Register<AzureDataStore>();
 
-            MainPage = new QuotesPage();
+            MainPage = new StackLayoutPage();
         }
 
         protected override void OnStart()
