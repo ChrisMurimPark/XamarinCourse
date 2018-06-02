a11d7a437c1732c3078dbe4723d4fdd0f6cbfc12
Chris (Murim) Park <chrispark@wustl.edu>
2018-06-02 16:20:46 -0500

    AbsoluteLayoutPage2 implemented

--- XamCourse/XamCourse/App.xaml.cs
+++ XamCourse/XamCourse/App.xaml.cs
@@ -22,7 +22,7 @@ namespace XamCourse
             //else
                 //DependencyService.Register<AzureDataStore>();
 
-            MainPage = new AbsoluteLayoutPage();
+            MainPage = new AbsoluteLayoutPage2();
         }
 
         protected override void OnStart()
---
+++ XamCourse/XamCourse/Views/AbsoluteLayoutPage2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+using Xamarin.Forms;
+
+namespace XamCourse.Views
+{
+    public partial class AbsoluteLayoutPage2 : ContentPage
+    {
+        public AbsoluteLayoutPage2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    var safe_insets = On<Xamarin.Forms.PlatformConfiguration.iOS>().SafeAreaInsets();
+                    safe_insets.Left = 20;
+                    safe_insets.Right = 20;
+                    safe_insets.Top += 20;
+                    relax_label.Margin = safe_insets;
+                    break;
+            }
+        }
+
+    }
+}
